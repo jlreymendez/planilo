120648c142fb59d98d6f46f9e7526689d957b235
Jose Luis Rey Mendez <jlreymendez@gmail.com>
2020-08-21 15:17:38 -0300

    Changed editor nodes have a specific color when they are a root or inactive

--- BehaviourTree/Editor/BehaviourTreeGraphNodeEditor.cs
+++ BehaviourTree/Editor/BehaviourTreeGraphNodeEditor.cs
@@ -8,24 +8,33 @@ namespace Planilo.BT.Editor
     [CustomNodeEditor(typeof(BehaviourTreeGraphNode))]
     public class BehaviourTreeGraphNodeEditor : NodeEditor
     {
+        Color rootColor = new Color(0.42f, 0.18f, 0.18f);
+        Color inactiveColor = new Color(0.5f, 0.5f, 0.5f);
         Color runningColor = new Color(0.43f, 0.41f, 0.18f);
 
         public override Color GetTint()
         {
             // Make sure node is connected.
             var node = target as BehaviourTreeGraphNode;
-            if (node.Index < 0) return base.GetTint();
+            if (node.Index < 0) return GetEditorTint();
 
             // Check if there is an active game object with a runner.
             IAIBehaviourRunner<BehaviourTreeState> runner = Selection.activeGameObject != null ?
                 Selection.activeGameObject.GetComponent<IAIBehaviourRunner<BehaviourTreeState>>() : null;
-            if (runner == null) return base.GetTint();
+            if (runner == null) return GetEditorTint();
 
             // Check if it is running.
             var runnerState = runner.GetState();
             var state = runnerState[node.Index];
 
-            return state.LastUpdateTime == Time.time ? runningColor : base.GetTint();
+            return state.LastUpdateTime == Time.time ? runningColor : GetEditorTint();
+        }
+
+        Color GetEditorTint()
+        {
+            var node = target as BehaviourTreeGraphNode;
+            var hasParent = node.GetInputPort("parent").Connection != null;
+            return node.IsRoot ? rootColor : hasParent ? base.GetTint() : base.GetTint() * inactiveColor;
         }
     }
 }
